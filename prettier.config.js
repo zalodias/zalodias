3929ed37ece2f631b1a19b2d80aabafbfbd5f5d2
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-03-24 11:59:58 +0000

    :wrench: Configure Prettier formatter

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  printWidth: 80,
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+};
