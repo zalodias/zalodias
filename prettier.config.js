c4b6ee1e4528ab585a594745f888b120eacaf0c2
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-09-28 10:18:58 +0100

    🔧 Add Prettier configuration

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("prettier").Config} */
-module.exports = {
-  plugins: ['prettier-plugin-tailwindcss'],
-  printWidth: 80,
-  semi: false,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "all",
-};
