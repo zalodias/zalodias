bdefdd5040d1290e8cc410f93682b72362d969b8
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-09-28 10:33:19 +0100

    🎨 Simplify page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,10 @@
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Inter as FontSans } from 'next/font/google'
+import '@/styles/globals.css';
+import { PropsWithChildren } from 'react';
 
-import { cn } from '@/lib/utils'
-
-const fontSans = FontSans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-})
-
-export const metadata: Metadata = {
-  title: 'Gonçalo Dias',
-}
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+export default function Layout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body
-        className={cn('min-h-screen font-sans antialiased', fontSans.variable)}
-      >
-        {children}
-      </body>
+      <body className="min-h-screen antialiased">{children}</body>
     </html>
-  )
+  );
 }
