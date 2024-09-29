46cd22035d1311f542de0c829a823e60c4b560f9
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-09-29 23:13:14 +0100

    ✨ Add navigation to base layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
-import '@/globals.css';
+import '@/app/globals.css';
+import { Navigation } from '@/components/navigation';
 import { PropsWithChildren } from 'react';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className="min-h-screen antialiased">{children}</body>
+      <body className="flex min-h-screen gap-4 bg-background-neutral-default text-foreground-neutral-default antialiased">
+        <Navigation />
+        <main className="flex-grow p-4">{children}</main>
+      </body>
     </html>
   );
 }
