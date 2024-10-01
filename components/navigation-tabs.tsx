53ebd7a5284fecfd0b144391a29dc13a18b94926
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-10-01 23:14:45 +0100

    ✨ Create component for navigation tabs to handle conditional client side logic

---
+++ components/navigation-tabs.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { navigation } from '@/data/navigation';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+export function NavigationTabs() {
+  const pathname = usePathname();
+
+  return (
+    <div className="flex flex-grow flex-col gap-2">
+      {navigation.map((item) => {
+        const isActive = pathname === item.path;
+        return (
+          <Link
+            key={item.path}
+            href={item.path}
+            className={`flex gap-3 rounded-lg border px-3 py-2 ${
+              isActive
+                ? 'border-border-neutral-default bg-background-neutral-subtle text-foreground-neutral-default'
+                : 'border-transparent text-foreground-neutral-faded hover:text-foreground-neutral-default'
+            }`}
+          >
+            {item.icon}
+            {item.label}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
