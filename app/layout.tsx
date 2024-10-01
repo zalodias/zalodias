cc1e0deb4400de4e5bb1e7d9874bfbb83be1bbce
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-10-02 00:33:43 +0100

    💄 Adjust layout padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: PropsWithChildren) {
     <html lang="en">
       <body className="flex min-h-screen gap-4 bg-background-neutral-default text-foreground-neutral-default antialiased">
         <Navigation />
-        <main className="flex-grow p-4">{children}</main>
+        <main className="flex flex-grow px-5 py-10">{children}</main>
       </body>
     </html>
   );
