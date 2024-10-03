d3b1e170b3e9eabaf7d8232a69d49dc8fe27e3fa
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-10-04 00:16:36 +0100

    💄 Prevent overscroll behavior

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { PropsWithChildren } from 'react';
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen gap-4 bg-background-neutral-default text-foreground-neutral-default antialiased">
+      <body className="flex gap-5 overscroll-none bg-background-neutral-default text-foreground-neutral-default antialiased">
         <Navigation />
         <main className="flex flex-grow px-5 py-10">{children}</main>
       </body>
