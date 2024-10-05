091c150bb8f8e8c149c0e681c54092b0d80d8b99
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-10-05 22:45:59 +0100

    ✨ Add Umami script for website analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/app/globals.css';
 import { Navigation } from '@/components/navigation';
+import Script from 'next/script';
 import { PropsWithChildren } from 'react';
 
 export default function Layout({ children }: PropsWithChildren) {
@@ -9,6 +10,10 @@ export default function Layout({ children }: PropsWithChildren) {
         <Navigation />
         <main className="flex flex-grow px-5 py-10">{children}</main>
       </body>
+      <Script
+        src="https://cloud.umami.is/script.js"
+        data-website-id="49a480db-8f0e-4ed8-ac4e-3fd0036c097e"
+      />
     </html>
   );
 }
