6445b23f7da6c26920c1c4411e64eb0dec583835
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-10-27 21:22:59 +0000

    ✨ Add time-based revalidation for timezone fetch

--- lib/ipapi.ts
+++ lib/ipapi.ts
@@ -1,14 +1,16 @@
 export async function getHoursFromTimezone(): Promise<number | string> {
   try {
-    const response = await fetch('https://ipapi.co/json/');
+    const response = await fetch('https://ipapi.co/json/', {
+      next: { revalidate: 3600 },
+    });
     const data = await response.json();
     const timezone = data.timezone;
 
     const date = new Date().toLocaleString('en-US', { timeZone: timezone });
-    const time = new Date(date).getHours();
+    const hours = new Date(date).getHours();
 
-    return time;
+    return hours;
   } catch (error) {
-    return 'Failed to fetch time';
+    return 'Failed to get hours from timezone';
   }
 }
