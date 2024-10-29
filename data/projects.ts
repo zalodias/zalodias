e852dff17c81001707a788ac4a05c445ed5d8662
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-10-29 22:43:22 +0000

    ✨ Add sample projects data

---
+++ data/projects.ts
@@ -0,0 +1,27 @@
+export interface Project {
+  title: string;
+  description: string;
+  image: string;
+}
+
+export const projects: Project[] = [
+  {
+    title: 'PDF BFF',
+    description:
+      'Enhancing documents with AI – transforming knowledge into art.',
+    image:
+      'https://framerusercontent.com/images/SUlDuUNR9V2gOJYXxGnQ8gEQyk.webp',
+  },
+  {
+    title: 'Cronio',
+    description: 'Time tracking, made easy.',
+    image:
+      'https://framerusercontent.com/images/TuKlBIjYU2SUMUdZjRk89Hsslhs.webp',
+  },
+  {
+    title: 'Autumnal Peach',
+    description: 'A project made with love, and a lot of coffee.',
+    image:
+      'https://framerusercontent.com/images/6prmhvfmhanjxVLvZZpcJn7WllA.webp',
+  },
+];
