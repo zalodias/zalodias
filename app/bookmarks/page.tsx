a678ddd57d352efedfaa9f203cf7e01908225acd
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-10-30 20:10:14 +0000

    🎨 Simplify bookmark list for initial release

--- app/bookmarks/page.tsx
+++ app/bookmarks/page.tsx
@@ -1,30 +1,11 @@
 import { Container } from '@/components/container';
 import { Intro } from '@/components/intro';
 import { getBookmarks } from '@/lib/notion';
-import {
-  extractFaviconFromUrl,
-  formatBookmarkDate,
-  getMonthName,
-} from '@/lib/utils';
+import { extractFaviconFromUrl, formatBookmarkDate } from '@/lib/utils';
 import { ArrowUpRight } from 'lucide-react';
 
 export default async function Bookmarks() {
   const bookmarks = await getBookmarks();
-  console.log(bookmarks);
-
-  const groupedBookmarks = bookmarks.reduce(
-    (acc, item) => {
-      const date = new Date(item.created_time);
-      const monthYear = `${getMonthName(date.getMonth() + 1)} ${date.getFullYear()}`;
-
-      if (!acc[monthYear]) {
-        acc[monthYear] = [];
-      }
-      acc[monthYear].push(item);
-      return acc;
-    },
-    {} as Record<string, typeof bookmarks>,
-  );
 
   return (
     <Container>
@@ -33,44 +14,39 @@ export default async function Bookmarks() {
           title="Bookmarks"
           description="A curated list of my favorite resources on the web. Updated regularly."
         />
-        {Object.entries(groupedBookmarks).map(([monthYear, items]) => (
-          <div key={monthYear} className="flex flex-col gap-4">
-            <div className="flex justify-between">
-              <h2 className="text-title-small-strong">{monthYear}</h2>
-              <p className="text-body-medium-subtle text-foreground-neutral-subtle">
-                {items.length} bookmarks
-              </p>
-            </div>
-            <div className="flex flex-col gap-1">
-              {items.map((item) => (
-                <a
-                  className="group flex items-center gap-2 rounded-lg p-2 hover:bg-background-neutral-faded"
-                  href={item.properties.Link.url}
-                  key={item.id}
-                  target="_blank"
-                >
-                  <div className="flex flex-grow items-center gap-1">
-                    <img
-                      src={extractFaviconFromUrl(item.properties.Link.url, 64)}
-                      alt="favicon"
-                      className="size-4"
-                    />
-                    <p className="text-body-large-strong">
-                      {item.properties.Name.title[0].plain_text}
-                    </p>
-                    <ArrowUpRight
-                      width={16}
-                      className="scale-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100"
-                    />
-                  </div>
-                  <p className="text-body-medium-subtle text-foreground-neutral-faded">
-                    {formatBookmarkDate(item.created_time)}
+        <div className="flex flex-col gap-1">
+          {bookmarks.map((bookmark) => (
+            <div key={bookmark.id} className="flex flex-col gap-4">
+              <a
+                className="group flex items-center gap-2 rounded-lg p-2 hover:bg-background-neutral-faded"
+                href={bookmark.properties.Link.url}
+                key={bookmark.id}
+                target="_blank"
+              >
+                <div className="flex flex-grow items-center gap-1">
+                  <img
+                    src={extractFaviconFromUrl(
+                      bookmark.properties.Link.url,
+                      64,
+                    )}
+                    alt="favicon"
+                    className="size-4"
+                  />
+                  <p className="text-body-large-strong">
+                    {bookmark.properties.Name.title[0].plain_text}
                   </p>
-                </a>
-              ))}
+                  <ArrowUpRight
+                    width={16}
+                    className="scale-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100"
+                  />
+                </div>
+                <p className="text-body-medium-subtle text-foreground-neutral-faded">
+                  {formatBookmarkDate(bookmark.created_time)}
+                </p>
+              </a>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </section>
     </Container>
   );
