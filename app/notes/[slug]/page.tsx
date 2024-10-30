d26725c8feba3d374f1210daed61e2175ed08cdb
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-10-30 22:31:54 +0000

    ✨ Add dynamic route for individual note

---
+++ app/notes/[slug]/page.tsx
@@ -0,0 +1,31 @@
+import { Container } from '@/components/container';
+import { fetchPageContent } from '@/lib/notion';
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+
+  const blocks = await fetchPageContent('0331d48a1387493b8b68141b7ee4d8d0');
+
+  return (
+    <Container>
+      <section className="flex flex-col gap-10">
+        <h1 className="text-title-large-strong">{slug}</h1>
+        <div className="flex flex-col gap-2">
+          {blocks.map((block: any) => {
+            return (
+              <p key={block.id}>
+                {block.paragraph.rich_text.map(
+                  (text: any) => text.text.content,
+                )}
+              </p>
+            );
+          })}
+        </div>
+      </section>
+    </Container>
+  );
+}
