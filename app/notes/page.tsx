70dcd4c86e9a1d9ca0fbbad3dff51e2e7ae0d10e
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-10-30 22:31:28 +0000

    ✨ Add notes page content

---
+++ app/notes/page.tsx
@@ -0,0 +1,33 @@
+import { Container } from '@/components/container';
+import { Intro } from '@/components/intro';
+import { fetchDatabaseContent } from '@/lib/notion';
+import { generateSlug } from '@/lib/utils';
+import Link from 'next/link';
+
+export default async function Notes() {
+  const notes = await fetchDatabaseContent('12057c1e9612806bb6cef4c32590960f');
+
+  return (
+    <Container>
+      <section className="flex flex-col gap-20">
+        <Intro
+          title="Notes"
+          description="A collection of crystallized thoughts and ideas."
+        />
+        <div className="flex flex-col gap-1">
+          {notes.map((note) => (
+            <Link
+              key={note.id}
+              href={`/notes/${generateSlug(note.properties.Name.title[0].plain_text)}`}
+              className="flex flex-col gap-4"
+            >
+              <p className="text-body-large-strong">
+                {note.properties.Name.title[0].plain_text}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </section>
+    </Container>
+  );
+}
