78d681b690f1df17efa92bb787467962b712fc12
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-11-01 22:04:08 +0000

    ✨ Fetch project data from Notion database

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,9 +1,13 @@
 import { Container } from '@/components/container';
 import { Intro } from '@/components/intro';
-import { projects } from '@/data/projects';
+import { fetchDatabaseContent } from '@/lib/notion';
 import Image from 'next/image';
 
-export default function Projects() {
+export default async function Projects() {
+  const projects = await fetchDatabaseContent(
+    '13157c1e961280e99dfffd55147e74b3',
+  );
+
   return (
     <Container>
       <section className="flex flex-col gap-20">
@@ -13,19 +17,24 @@ export default function Projects() {
         />
         <section className="grid grid-cols-[repeat(auto-fill,minmax(400px,1fr))] gap-20">
           {projects.map((project) => (
-            <div key={project.title} className="flex flex-col gap-4">
+            <div key={project.id} className="flex flex-col gap-4">
               <div className="relative block h-80 w-full">
                 <Image
-                  src={project.image}
-                  alt={project.title}
+                  src={''}
+                  alt={(project.properties.Name as any).title[0].plain_text}
                   fill
-                  className="rounded-xl object-cover"
+                  className="rounded-xl bg-background-neutral-faded object-cover"
                 />
               </div>
               <div className="flex flex-col gap-1">
-                <h2 className="text-title-small-strong">{project.title}</h2>
-                <p className="text-body-medium-subtle text-foreground-neutral-faded">
-                  {project.description}
+                <h2 className="text-title-small-strong">
+                  {(project.properties.Name as any).title[0].plain_text}
+                </h2>
+                <p className="text-body-large-default text-foreground-neutral-faded">
+                  {
+                    (project.properties.Description as any).rich_text[0]
+                      .plain_text
+                  }
                 </p>
               </div>
             </div>
