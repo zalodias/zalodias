0a8cf7a6552a3c9acfe53ecaa930cdfdb38092e0
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-11-09 21:56:11 +0000

    ✨ Add dynamic project detail data from Notion

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import { Container } from '@/components/container';
-import { ProjectMetadata } from '@/components/project-metadata';
 import { fetchDatabaseContent, fetchPageContent } from '@/lib/notion';
 import { generateSlug } from '@/lib/utils';
 import Image from 'next/image';
@@ -26,36 +25,53 @@ export default async function Project({
   return (
     <Container>
       <div className="flex flex-col gap-10">
+        <div className="relative left-1/2 w-screen -translate-x-1/2 overflow-clip bg-background-neutral-faded lg:static lg:left-0 lg:order-1 lg:w-full lg:translate-x-0 lg:rounded-xl">
+          <Image
+            src={page.cover.file.url}
+            alt={(page.properties.Name as any).title[0].plain_text}
+            width={1920}
+            height={1440}
+            className="object-cover"
+          />
+        </div>
         <header className="flex items-start justify-between gap-20">
           <div className="flex flex-col gap-3">
             <h1 className="text-title-large-strong">
-              Paving the way for a new system of collaboration
+              {(page.properties.Name as any).title[0].plain_text}
             </h1>
             <p className="text-body-large-default text-foreground-neutral-subtle">
-              Kübe is a newly created co-work space in Lisbon. The identity
-              concept displays the desire to present this space as dynamic and
-              inclusive, combined with a technological and modern vision. Its
-              symbol is a versatile representation, flexible to content.
+              {(page.properties.Description as any).rich_text[0].plain_text}
             </p>
           </div>
-          <button className="inline-flex items-center justify-center gap-2 rounded-lg border border-border-neutral-faded bg-background-neutral-faded px-3 py-2 text-body-medium-subtle text-foreground-neutral-faded hover:bg-background-neutral-subtle hover:text-foreground-neutral-default disabled:pointer-events-none disabled:opacity-50">
-            Share
-          </button>
         </header>
-        <section className="flex gap-20">
-          <ProjectMetadata title="Year" description="2024" />
-          <ProjectMetadata title="Role" description="Product Design" />
-          <ProjectMetadata title="Deliverable" description="Mobile App" />
+        <section className="flex flex-wrap gap-10 md:gap-20">
+          <div className="flex flex-col gap-2">
+            <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
+              Year
+            </p>
+            <p className="text-body-medium-subtle">
+              {page.properties.Year.rich_text[0].plain_text}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
+              Deliverable
+            </p>
+            <p className="text-body-medium-subtle">
+              {page.properties.Deliverable.select.name}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
+              Skills
+            </p>
+            <p className="text-body-medium-subtle">
+              {page.properties.Focus.multi_select
+                .map((skill) => skill.name)
+                .join(', ')}
+            </p>
+          </div>
         </section>
-        <div className="flex flex-col gap-10">
-          <Image
-            src="https://framerusercontent.com/images/sEbxQ4942NuQ3nev9zvvPxvpn0.jpg"
-            alt="Project"
-            className="rounded-xl"
-            width={960}
-            height={640}
-          />
-        </div>
       </div>
     </Container>
   );
