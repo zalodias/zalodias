80e2e0fcba36c39a587817959639340b57d67051
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-11-09 21:49:18 +0000

    ✨ Add bottom padding for mobile tab bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export default function Layout({ children }: PropsWithChildren) {
         <Header />
         <Footer />
         <Sidebar />
-        <main className="flex flex-grow px-5 py-10 md:px-10">{children}</main>
+        <main className="flex flex-grow px-5 pb-36 pt-10 lg:px-10 lg:pb-10">
+          {children}
+        </main>
       </body>
       <Script
         src="https://cloud.umami.is/script.js"
