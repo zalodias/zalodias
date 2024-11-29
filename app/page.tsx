fc15e3a5932ba089152b13206e112be78546f6ba
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-11-29 09:10:47 +0000

    ✨ Update Explore page with real, fetched content

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,32 @@
-import { AppIcon } from '@/components/app-icon';
-import { BookmarkCard } from '@/components/bookmark-card';
 import { Container } from '@/components/container';
 import { MovingArrow } from '@/components/moving-arrow';
 import { NoteCard } from '@/components/note-card';
-import { ProjectCard } from '@/components/project-card';
-import { Arc } from '@/icons/Arc';
-import { Linear } from '@/icons/Linear';
-import { Raycast } from '@/icons/Raycast';
 import { getHoursFromTimezone } from '@/lib/ipapi';
+import { fetchDatabaseContent } from '@/lib/notion';
 import { getPageVisitorCount } from '@/lib/umami';
-import { getGreetingFromHours } from '@/lib/utils';
+import {
+  extractFaviconFromUrl,
+  formatBookmarkDate,
+  generateSlug,
+  getGreetingFromHours,
+} from '@/lib/utils';
+import { ArrowUpRight } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default async function Home() {
   const hours = await getHoursFromTimezone();
   const greeting = typeof hours === 'number' && getGreetingFromHours(hours);
+
+  const projects = await fetchDatabaseContent(
+    '13157c1e961280e99dfffd55147e74b3',
+  );
+
+  const bookmarks = await fetchDatabaseContent(
+    '12057c1e961280329ebad0ecdf335eb7',
+  );
+
+  const notes = await fetchDatabaseContent('12057c1e9612806bb6cef4c32590960f');
 
   return (
     <Container>
@@ -29,118 +41,109 @@ export default async function Home() {
       </div>
       <div className="flex flex-col gap-6">
         <div className="flex gap-4">
-          <h1 className="flex-grow text-title-small-strong">Featured Work</h1>
+          <h1 className="flex-grow text-title-medium-strong">Featured Work</h1>
+        </div>
+        <section className="grid gap-10 md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))]">
+          {projects.map((project) => (
+            <Link
+              key={project.id}
+              className="flex flex-col gap-4"
+              href={`/projects/${generateSlug((project.properties.Name as any).title[0].plain_text)}`}
+            >
+              <div className="relative block w-full overflow-clip rounded-xl bg-background-neutral-faded">
+                <Image
+                  src={(project.cover as any).file.url}
+                  alt={(project.properties.Name as any).title[0].plain_text}
+                  width={1920}
+                  height={1440}
+                  className="object-cover duration-200 hover:scale-105"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <h2 className="text-title-small-strong">
+                  {(project.properties.Name as any).title[0].plain_text}
+                </h2>
+                <p className="text-body-large-default text-foreground-neutral-faded">
+                  {
+                    (project.properties.Description as any).rich_text[0]
+                      .plain_text
+                  }
+                </p>
+              </div>
+            </Link>
+          ))}
+        </section>
+      </div>
+      <section className="flex flex-col gap-6">
+        <header className="flex gap-4">
+          <h1 className="flex-grow text-title-medium-strong">Bookmarks</h1>
           <Link
-            href="/projects"
+            href="/bookmarks"
             className="group flex items-center gap-2 text-body-medium-subtle text-foreground-neutral-faded hover:text-foreground-neutral-default"
           >
             View All
             <MovingArrow />
           </Link>
-        </div>
-        <div className="grid grid-flow-row gap-10 lg:grid-flow-col">
-          <ProjectCard
-            title="Autumnal Peach"
-            description="A project made with love, and a lot of coffee."
-            image="https://framerusercontent.com/images/sEbxQ4942NuQ3nev9zvvPxvpn0.jpg"
-            link="/projects/autumnal-peach"
-          />
-          <ProjectCard
-            image="https://framerusercontent.com/images/sEbxQ4942NuQ3nev9zvvPxvpn0.jpg"
-            title="Autumnal Peach"
-            description="A project made with love, and a lot of coffee."
-            link="/projects/autumnal-peach"
-          />
-          <ProjectCard
-            image="https://framerusercontent.com/images/sEbxQ4942NuQ3nev9zvvPxvpn0.jpg"
-            title="Autumnal Peach"
-            description="A project made with love, and a lot of coffee."
-            link="/projects/autumnal-peach"
-          />
-        </div>
-      </div>
-      <div className="flex flex-col gap-8 rounded-xl border border-border-neutral-faded p-6">
-        <header className="flex flex-col gap-1">
-          <h1 className="text-title-small-strong">Dissections</h1>
-          <p className="text-foreground-neutral-faded">
-            Analysing the delightful details of my favorite software
-          </p>
         </header>
-        <div className="grid w-full grid-cols-2 gap-4">
-          <AppIcon
-            icon={<Linear />}
-            title="Linear"
-            description="8 details"
-            className="w-full"
-          />
-          <AppIcon
-            icon={<Arc />}
-            title="Arc"
-            description="5 details"
-            className="w-full"
-          />
-          <AppIcon
-            icon={<Raycast />}
-            title="Raycast"
-            description="3 details"
-            className="w-full"
-          />
-          <AppIcon
-            icon={<Linear />}
-            title="Linear"
-            description="8 details"
-            className="w-full"
-          />
-        </div>
-        <Link
-          href="/dissections"
-          className="inline-flex items-center justify-center gap-2 rounded-lg border border-border-neutral-faded bg-background-neutral-faded px-5 py-3 text-body-medium-subtle text-foreground-neutral-faded hover:bg-background-neutral-subtle hover:text-foreground-neutral-default disabled:pointer-events-none disabled:opacity-50"
-        >
-          View Dissections
-        </Link>
-      </div>
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-4">
-          <h1 className="flex-grow text-title-small-strong">Bookmarks</h1>
-          <Link
-            href="/projects"
-            className="group flex items-center gap-2 text-body-medium-subtle text-foreground-neutral-faded hover:text-foreground-neutral-default"
-          >
-            View All
-            <MovingArrow />
-          </Link>
-        </div>
         <div className="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-4">
-          <BookmarkCard />
-          <BookmarkCard />
-          <BookmarkCard />
-          <BookmarkCard />
-          <BookmarkCard />
+          {bookmarks.slice(0, 6).map((bookmark) => (
+            <div key={bookmark.id} className="flex flex-col gap-4">
+              <a
+                className="group relative flex flex-grow flex-col gap-3 rounded-lg border border-border-neutral-faded bg-background-neutral-faded px-4 py-3 hover:bg-background-neutral-subtle"
+                href={(bookmark.properties.Link as any).url}
+                key={bookmark.id}
+                target="_blank"
+              >
+                <img
+                  src={extractFaviconFromUrl(
+                    (bookmark.properties.Link as any).url,
+                    64,
+                  )}
+                  alt="favicon"
+                  className="size-4"
+                />
+                <div className="flex flex-grow flex-col justify-between gap-2">
+                  <p className="text-body-large-strong">
+                    {(bookmark.properties.Name as any).title[0].plain_text}
+                  </p>
+                  <p className="text-body-medium-subtle text-foreground-neutral-faded">
+                    {formatBookmarkDate(bookmark.created_time)}
+                  </p>
+                </div>
+                <ArrowUpRight
+                  width={16}
+                  className="absolute right-2 top-2 scale-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100"
+                />
+              </a>
+            </div>
+          ))}
         </div>
-      </div>
-      <div className="rounded-xl border border-border-neutral-faded p-6">
-        <header className="flex flex-col gap-5">
-          <h1 className="text-title-small-strong">Recent Notes</h1>
-          <div className="flex flex-col gap-3">
-            <NoteCard
-              title="Moebius-style post-processing"
-              timestamp="Just now"
-            />
-            <NoteCard
-              title="Docker container management tools"
-              timestamp="20 minutes ago"
-            />
-            <NoteCard
-              title="Moebius-style post-processing"
-              timestamp="1 week ago"
-            />
-            <NoteCard
-              title="Consistent toolset for manipulating JavaScript dates"
-              timestamp="2 months ago"
-            />
-          </div>
+      </section>
+      <section className="flex flex-col gap-6">
+        <header className="flex gap-4">
+          <h1 className="flex-grow text-title-medium-strong">Notes</h1>
+          <Link
+            href="/notes"
+            className="group flex items-center gap-2 text-body-medium-subtle text-foreground-neutral-faded hover:text-foreground-neutral-default"
+          >
+            View All
+            <MovingArrow />
+          </Link>
         </header>
-      </div>
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(320px,1fr))] gap-4">
+          {notes.slice(0, 4).map((note) => (
+            <Link
+              key={note.id}
+              href={`/notes/${generateSlug((note.properties.Name as any).title[0].plain_text)}`}
+              className="flex flex-col gap-2 rounded-lg border border-border-neutral-faded bg-background-neutral-faded p-4 hover:bg-background-neutral-subtle"
+            >
+              <p className="text-title-small-strong">
+                {(note.properties.Name as any).title[0].plain_text}
+              </p>
+            </Link>
+          ))}
+        </div>
+      </section>
     </Container>
   );
 }
