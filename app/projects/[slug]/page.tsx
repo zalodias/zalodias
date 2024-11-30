2e2f30fd24c995be030baa7aff266aaccffddcbd
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-11-30 18:29:18 +0000

    ✨ Improve project page layout and integrate NotionBlock for dynamic content rendering

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,5 +1,10 @@
 import { Container } from '@/components/container';
-import { fetchDatabaseContent, fetchPageContent } from '@/lib/notion';
+import { NotionBlock } from '@/components/notion-block';
+import {
+  fetchBlockContent,
+  fetchDatabaseContent,
+  fetchPageContent,
+} from '@/lib/notion';
 import { generateSlug } from '@/lib/utils';
 import Image from 'next/image';
 
@@ -14,6 +19,8 @@ export default async function Project({
     '13157c1e961280e99dfffd55147e74b3',
   );
 
+  const blocks = await fetchBlockContent('9d1c3ed50fc349af8242f2d9da34d675');
+
   const id = database.find(
     (project) =>
       generateSlug((project.properties.Name as any).title[0].plain_text) ===
@@ -23,53 +30,60 @@ export default async function Project({
   const page = await fetchPageContent(id!);
 
   return (
-    <Container className="gap-10 pt-0 lg:pt-10">
-      <div className="relative left-1/2 w-screen -translate-x-1/2 overflow-clip bg-background-neutral-faded lg:static lg:left-0 lg:order-1 lg:w-full lg:translate-x-0 lg:rounded-xl">
-        <Image
-          src={(page.cover as any).file.url}
-          alt={(page.properties.Name as any).title[0].plain_text}
-          width={1920}
-          height={1440}
-          className="object-cover"
-        />
-      </div>
-      <section className="flex items-start justify-between gap-20">
-        <div className="flex flex-col gap-3">
-          <h1 className="text-title-large-strong">
-            {(page.properties.Name as any).title[0].plain_text}
-          </h1>
-          <p className="text-body-large-default text-foreground-neutral-subtle">
-            {(page.properties.Description as any).rich_text[0].plain_text}
-          </p>
+    <Container className="pt-0 lg:pt-10">
+      <section className="flex flex-col gap-10">
+        <div className="relative left-1/2 w-screen -translate-x-1/2 overflow-clip bg-background-neutral-faded lg:static lg:left-0 lg:order-1 lg:w-full lg:translate-x-0 lg:rounded-xl">
+          <Image
+            src={(page.cover as any).file.url}
+            alt={(page.properties.Name as any).title[0].plain_text}
+            width={1920}
+            height={1440}
+            className="object-cover"
+          />
         </div>
+        <header className="flex items-start justify-between gap-20">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-title-large-strong">
+              {(page.properties.Name as any).title[0].plain_text}
+            </h1>
+            <p className="text-body-large-default text-foreground-neutral-subtle">
+              {(page.properties.Description as any).rich_text[0].plain_text}
+            </p>
+          </div>
+        </header>
+        <section className="flex flex-wrap gap-10 md:gap-20">
+          <div className="flex flex-col gap-2">
+            <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
+              Year
+            </p>
+            <p className="text-body-medium-subtle">
+              {(page.properties.Year as any).rich_text[0].plain_text}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
+              Deliverable
+            </p>
+            <p className="text-body-medium-subtle">
+              {(page.properties.Deliverable as any).select.name}
+            </p>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
+              Skills
+            </p>
+            <p className="text-body-medium-subtle">
+              {(page.properties.Skills as any).multi_select
+                .map((skill: { name: string }) => skill.name)
+                .join(', ')}
+            </p>
+          </div>
+        </section>
       </section>
-      <section className="flex flex-wrap gap-10 md:gap-20">
-        <div className="flex flex-col gap-2">
-          <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
-            Year
-          </p>
-          <p className="text-body-medium-subtle">
-            {(page.properties.Year as any).rich_text[0].plain_text}
-          </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
-            Deliverable
-          </p>
-          <p className="text-body-medium-subtle">
-            {(page.properties.Deliverable as any).select.name}
-          </p>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
-            Skills
-          </p>
-          <p className="text-body-medium-subtle">
-            {(page.properties.Skills as any).multi_select
-              .map((skill: { name: string }) => skill.name)
-              .join(', ')}
-          </p>
-        </div>
+      <section>
+        {blocks.map((block: any) => (
+          <NotionBlock key={block.id} block={block} />
+        ))}
       </section>
     </Container>
   );
