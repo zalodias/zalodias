9fd5cec17e295a112666f5580f977ce745aafefb
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-11-30 18:48:11 +0000

    💄 Add rich text styling for Notion blocks

--- components/notion-block.tsx
+++ components/notion-block.tsx
@@ -12,25 +12,37 @@ export function NotionBlock({ block }: NotionBlockProps) {
   switch (block.type) {
     case 'paragraph':
       return (
-        <p key={block.id}>
+        <p
+          key={block.id}
+          className="text-body-large-default text-foreground-neutral-subtle"
+        >
           {block.paragraph.rich_text.map((text: any) => text.text.content)}
         </p>
       );
     case 'heading_1':
       return (
-        <h1 key={block.id}>
+        <h1
+          key={block.id}
+          className="mb-3 text-title-large-strong text-foreground-neutral-default"
+        >
           {block.heading_1.rich_text.map((text: any) => text.text.content)}
         </h1>
       );
     case 'heading_2':
       return (
-        <h2 key={block.id}>
+        <h2
+          key={block.id}
+          className="mb-2 text-title-medium-strong text-foreground-neutral-default"
+        >
           {block.heading_2.rich_text.map((text: any) => text.text.content)}
         </h2>
       );
     case 'heading_3':
       return (
-        <h3 key={block.id}>
+        <h3
+          key={block.id}
+          className="mb-1 text-title-small-strong text-foreground-neutral-default"
+        >
           {block.heading_3.rich_text.map((text: any) => text.text.content)}
         </h3>
       );
