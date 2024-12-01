e4c2bb53dd6d2f83ecbd9b425f186048937ba7d8
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-12-01 00:58:38 +0000

    💄 Improve margin spacing for text blocks

--- components/notion-block.tsx
+++ components/notion-block.tsx
@@ -41,7 +41,7 @@ export function NotionBlock({ block }: NotionBlockProps) {
       return (
         <h1
           key={block.id}
-          className="mb-3 text-title-large-strong text-foreground-neutral-default"
+          className="mb-4 mt-10 text-title-large-strong text-foreground-neutral-default"
         >
           {renderRichText(block.heading_1.rich_text)}
         </h1>
@@ -50,7 +50,7 @@ export function NotionBlock({ block }: NotionBlockProps) {
       return (
         <h2
           key={block.id}
-          className="mb-2 text-title-medium-strong text-foreground-neutral-default"
+          className="mb-3 mt-9 text-title-medium-strong text-foreground-neutral-default"
         >
           {renderRichText(block.heading_2.rich_text)}
         </h2>
@@ -59,7 +59,7 @@ export function NotionBlock({ block }: NotionBlockProps) {
       return (
         <h3
           key={block.id}
-          className="mb-1 text-title-small-strong text-foreground-neutral-default"
+          className="mb-2 mt-8 text-title-small-strong text-foreground-neutral-default"
         >
           {renderRichText(block.heading_3.rich_text)}
         </h3>
