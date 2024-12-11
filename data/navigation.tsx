4135b398d5e64d7a7b272e810060114062be94e9
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-12-11 01:24:21 +0000

    ✨ Update navigation with Stack tab

--- data/navigation.tsx
+++ data/navigation.tsx
@@ -1,18 +1,20 @@
-import { Bookmark, Compass, Folders, ScrollText } from 'lucide-react';
+import { Bookmark, Compass, Folders, Layers2, ScrollText } from 'lucide-react';
 
 export interface NavigationItem {
   path: string;
   icon: React.ReactNode;
   label: string;
+  new?: boolean;
 }
 
 export const navigation: NavigationItem[] = [
   { path: '/', label: 'Explore', icon: <Compass width={20} /> },
   { path: '/projects', label: 'Projects', icon: <Folders width={20} /> },
-  {
-    path: '/notes',
-    label: 'Notes',
-    icon: <ScrollText width={20} />,
-  },
+  { path: '/stack', label: 'Stack', icon: <Layers2 width={20} /> },
+  // {
+  //   path: '/notes',
+  //   label: 'Notes',
+  //   icon: <ScrollText width={20} />,
+  // },
   { path: '/bookmarks', label: 'Bookmarks', icon: <Bookmark width={20} /> },
 ];
