da525ea9270f89d63a025f06650b533d2bb7474d
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-12-15 22:57:12 +0000

    ✨ Add shortcut option to navigation items

--- data/navigation.tsx
+++ data/navigation.tsx
@@ -1,20 +1,30 @@
-import { Bookmark, Compass, Folders, Layers2, ScrollText } from 'lucide-react';
+import { Bookmark, Compass, Folders, Layers2 } from 'lucide-react';
 
 export interface NavigationItem {
   path: string;
   icon: React.ReactNode;
   label: string;
-  new?: boolean;
+  shortcut?: string;
 }
 
 export const navigation: NavigationItem[] = [
-  { path: '/', label: 'Explore', icon: <Compass width={20} /> },
-  { path: '/projects', label: 'Projects', icon: <Folders width={20} /> },
-  { path: '/stack', label: 'Stack', icon: <Layers2 width={20} /> },
-  // {
-  //   path: '/notes',
-  //   label: 'Notes',
-  //   icon: <ScrollText width={20} />,
-  // },
-  { path: '/bookmarks', label: 'Bookmarks', icon: <Bookmark width={20} /> },
+  { path: '/', label: 'Explore', icon: <Compass width={20} />, shortcut: '1' },
+  {
+    path: '/projects',
+    label: 'Projects',
+    icon: <Folders width={20} />,
+    shortcut: '2',
+  },
+  {
+    path: '/stack',
+    label: 'Stack',
+    icon: <Layers2 width={20} />,
+    shortcut: '3',
+  },
+  {
+    path: '/bookmarks',
+    label: 'Bookmarks',
+    icon: <Bookmark width={20} />,
+    shortcut: '4',
+  },
 ];
