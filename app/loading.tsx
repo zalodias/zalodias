38359d90293bcda6b5cf2d05970d92909a48fd60
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-12-15 18:42:10 +0000

    💄 Adjust loader position for large breakpoint

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="absolute inset-0 grid place-items-center">
+    <div className="absolute inset-0 grid place-items-center lg:left-80">
       <div
         role="status"
         className="size-8 animate-spin rounded-full border-2 border-solid border-e-transparent text-foreground-neutral-default"
