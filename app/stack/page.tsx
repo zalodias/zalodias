c4363d89dc7e328e1150b5de58eb8379a0a5144d
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-12-17 23:07:27 +0000

    🎨 Refactor Stack page to fetch data from Notion API

--- app/stack/page.tsx
+++ app/stack/page.tsx
@@ -1,27 +1,34 @@
 import { Container } from '@/components/container';
 import { Intro } from '@/components/intro';
-import { stack } from '@/data/stack';
+import { fetchDatabaseContent } from '@/lib/notion';
 
-export default function Stack() {
+export default async function Stack() {
+  const stack = await fetchDatabaseContent('15d57c1e961280d1b3ccc835fa7a054b');
+
   return (
     <Container>
       <Intro title="Stack">A collection of my favorite working tools</Intro>
       <div className="grid gap-5 md:grid-cols-[repeat(auto-fill,minmax(320px,1fr))]">
         {stack.map((resource) => (
           <div
-            key={resource.name}
+            key={resource.id}
             className="flex flex-col gap-4 rounded-xl border border-border-neutral-faded bg-background-neutral-faded p-4"
           >
             <div className="size-14">
               <img
-                src={resource.image}
+                src={(resource.properties.Image as any).url}
                 className="rounded-xl border border-border-neutral-subtle"
               ></img>
             </div>
             <div className="grid gap-0.5">
-              <span className="text-body-large-strong">{resource.name}</span>
+              <span className="text-body-large-strong">
+                {(resource.properties.Name as any).title[0].plain_text}
+              </span>
               <p className="text-body-large-default text-foreground-neutral-subtle">
-                {resource.description}
+                {
+                  (resource.properties.Description as any).rich_text[0]
+                    .plain_text
+                }
               </p>
             </div>
           </div>
