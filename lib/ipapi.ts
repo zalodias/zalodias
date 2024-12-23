0827bc1182935dde3939790767d093f8cbb43c2c
Gonçalo Dias <goncalodias.studio@gmail.com>
2024-12-23 18:30:23 +0000

    🎨 Revert timezone fetching function to remove logging

--- lib/ipapi.ts
+++ lib/ipapi.ts
@@ -1,8 +1,4 @@
-export async function getHoursFromTimezone(): Promise<{
-  hours: number | string;
-  logs: string;
-}> {
-  let logs = '';
+export async function getHoursFromTimezone(): Promise<number | string> {
   try {
     const response = await fetch('https://ipapi.co/json/', {
       next: { revalidate: 60 },
@@ -10,16 +6,11 @@ export async function getHoursFromTimezone(): Promise<{
     const data = await response.json();
     const timezone = data.timezone;
 
-    logs += `Fetched timezone: ${timezone}\n`; // Log the fetched timezone
-
     const date = new Date().toLocaleString('en-US', { timeZone: timezone });
     const hours = new Date(date).getHours();
 
-    logs += `Current time in fetched timezone: ${date}\n`; // Log the current time
-
-    return { hours, logs };
+    return hours;
   } catch (error) {
-    logs += 'Failed to get hours from timezone\n';
-    return { hours: logs, logs };
+    return 'Failed to get hours from timezone';
   }
 }
