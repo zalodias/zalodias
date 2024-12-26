77d1b5163e97f432e53e0c179ed4cf1aa09c3fb0
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2024-12-26 13:32:04 +0000

    ✨ Add utility function for native relative time formatting

--- lib/utils.ts
+++ lib/utils.ts
@@ -46,6 +46,42 @@ export function formatDate(
   return date.toLocaleDateString('en-US', options);
 }
 
+export function getRelativeTimeString(
+  date: Date | number,
+  lang = navigator.language,
+): string {
+  const time = typeof date === 'number' ? date : date.getTime();
+  const delta = Math.round((time - Date.now()) / 1000);
+
+  const cutoffs = [
+    60,
+    3600,
+    86400,
+    86400 * 7,
+    86400 * 30,
+    86400 * 365,
+    Infinity,
+  ];
+
+  const units: Intl.RelativeTimeFormatUnit[] = [
+    'second',
+    'minute',
+    'hour',
+    'day',
+    'week',
+    'month',
+    'year',
+  ];
+
+  const unitIndex = cutoffs.findIndex((cutoff) => cutoff > Math.abs(delta));
+
+  const divisor = unitIndex ? cutoffs[unitIndex - 1] : 1;
+
+  const rtf = new Intl.RelativeTimeFormat(lang, { numeric: 'auto' });
+
+  return rtf.format(Math.floor(delta / divisor), units[unitIndex]);
+}
+
 export function extractFaviconFromUrl(url: string, size: number = 16): string {
   const faviconUrl = new URL(
     `https://t3.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${url}&size=${size}`,
