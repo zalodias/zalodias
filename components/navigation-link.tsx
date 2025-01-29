3531c2db4067e7380c4f689fb1fb89c0ac6703e4
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-01-29 23:24:51 +0000

    ✨ Add transition effect to navigation link hover state

--- components/navigation-link.tsx
+++ components/navigation-link.tsx
@@ -27,7 +27,7 @@ export function NavigationLink({
   return (
     <Link
       href={path}
-      className="flex items-center gap-3 rounded-lg border border-transparent px-3 py-2 text-foreground-neutral-faded hover:text-foreground-neutral-default aria-[current=page]:border-border-neutral-subtle aria-[current=page]:bg-background-neutral-subtle aria-[current=page]:text-foreground-neutral-default"
+      className="flex items-center gap-3 rounded-lg border border-transparent px-3 py-2 text-foreground-neutral-faded transition hover:text-foreground-neutral-default aria-[current=page]:border-border-neutral-subtle aria-[current=page]:bg-background-neutral-subtle aria-[current=page]:text-foreground-neutral-default"
       aria-label={label}
       aria-current={isActive ? 'page' : undefined}
     >
