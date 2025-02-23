c81eb317b72a84de56cd911da1748a54f8e71fe0
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-02-23 18:31:54 +0000

    ✨ Add Transition component

---
+++ components/transition.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { ReactNode } from 'react';
+
+export function Transition({
+  duration = 0.2,
+  delay = 0,
+  offset = 20,
+  children,
+}: {
+  duration?: number;
+  delay?: number;
+  offset?: number;
+  children: ReactNode;
+}) {
+  return (
+    <motion.div
+      variants={{
+        hidden: { opacity: 0, y: offset },
+        visible: { opacity: 1, y: 0 },
+      }}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ type: 'easeOut', duration, delay }}
+    >
+      {children}
+    </motion.div>
+  );
+}
