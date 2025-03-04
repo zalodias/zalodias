4cc664d9542550c16dbdcedf1fbcd373fc47b628
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-03-04 22:33:12 +0000

    🎨 Remove unnecessary section wrapper

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -12,55 +12,53 @@ export default async function Projects() {
 
   return (
     <Container>
-      <section className="flex flex-col gap-20">
-        <Intro title="Projects">
-          A showcase of my latest design work. Currently crafting thoughtful
-          digital experiences at{' '}
-          <a
-            href="https://www.concealed.com"
-            className="text-body-large-subtle text-foreground-brand-default hover:underline hover:underline-offset-2"
-            target="_blank"
-          >
-            Concealed
-          </a>
-          .
-        </Intro>
-        <section className="grid gap-20 md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))]">
-          {projects
-            .filter(
-              (project) =>
-                (project.properties.Status as any).select.name === 'Ready',
-            )
-            .reverse()
-            .map((project) => (
-              <Link
-                key={project.id}
-                className="flex flex-col gap-4"
-                href={`/projects/${generateSlug((project.properties.Name as any).title[0].plain_text)}`}
-              >
-                <div className="relative block w-full overflow-clip rounded-xl bg-background-neutral-faded">
-                  <Image
-                    src={(project.cover as any).external.url}
-                    alt={(project.properties.Name as any).title[0].plain_text}
-                    width={1920}
-                    height={1440}
-                    className="object-cover duration-200 hover:scale-105"
-                  />
-                </div>
-                <div className="flex flex-col gap-1">
-                  <h2 className="text-title-small-strong">
-                    {(project.properties.Name as any).title[0].plain_text}
-                  </h2>
-                  <p className="text-body-large-default text-foreground-neutral-faded">
-                    {
-                      (project.properties.Description as any).rich_text[0]
-                        .plain_text
-                    }
-                  </p>
-                </div>
-              </Link>
-            ))}
-        </section>
+      <Intro title="Projects">
+        A showcase of my latest design work. Currently crafting thoughtful
+        digital experiences at{' '}
+        <a
+          href="https://www.concealed.com"
+          className="text-body-large-subtle text-foreground-brand-default hover:underline hover:underline-offset-2"
+          target="_blank"
+        >
+          Concealed
+        </a>
+        .
+      </Intro>
+      <section className="grid gap-20 md:grid-cols-[repeat(auto-fill,minmax(400px,1fr))]">
+        {projects
+          .filter(
+            (project) =>
+              (project.properties.Status as any).select.name === 'Ready',
+          )
+          .reverse()
+          .map((project) => (
+            <Link
+              key={project.id}
+              className="flex flex-col gap-4"
+              href={`/projects/${generateSlug((project.properties.Name as any).title[0].plain_text)}`}
+            >
+              <div className="relative block w-full overflow-clip rounded-xl bg-background-neutral-faded">
+                <Image
+                  src={(project.cover as any).external.url}
+                  alt={(project.properties.Name as any).title[0].plain_text}
+                  width={1920}
+                  height={1440}
+                  className="object-cover duration-200 hover:scale-105"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <h2 className="text-title-small-strong">
+                  {(project.properties.Name as any).title[0].plain_text}
+                </h2>
+                <p className="text-body-large-default text-foreground-neutral-faded">
+                  {
+                    (project.properties.Description as any).rich_text[0]
+                      .plain_text
+                  }
+                </p>
+              </div>
+            </Link>
+          ))}
       </section>
     </Container>
   );
