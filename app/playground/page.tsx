f5fdfe4b7a08abc225c2852ad971d5642b795104
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-03-09 20:13:23 +0000

    ✨ Add Playground page

---
+++ app/playground/page.tsx
@@ -0,0 +1,10 @@
+import { Scene } from '@/assets/shaders/scene';
+import { Container } from '@/components/container';
+
+export default function Page() {
+  return (
+    <Container>
+      <Scene />
+    </Container>
+  );
+}
