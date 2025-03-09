bfe78ab41c7e25ff5ea89e531dc9086d6cf29be6
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-03-09 20:12:17 +0000

    ✨ Add scene component for shader rendering

---
+++ assets/shaders/scene.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import fragmentShader from '!!raw-loader!./fragment.glsl';
+import vertexShader from '!!raw-loader!./vertex.glsl';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Mesh, ShaderMaterial } from 'three';
+
+export function Cube() {
+  const mesh = useRef<Mesh>(null!);
+
+  useFrame((state) => {
+    const { clock } = state;
+    const material = mesh.current.material as ShaderMaterial;
+    material.uniforms.uTime.value = clock.getElapsedTime();
+  });
+
+  return (
+    <mesh ref={mesh}>
+      <boxGeometry args={[1, 1, 1]} />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={{
+          uResolution: { value: [window.innerWidth, window.innerHeight] },
+          uTime: { value: 0 },
+        }}
+      />
+    </mesh>
+  );
+}
+
+export function Scene() {
+  return (
+    <Canvas>
+      <Cube />
+    </Canvas>
+  );
+}
