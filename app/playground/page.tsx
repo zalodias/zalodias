21295922ba73de876e099a4422d767170d5c71fd
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-03-23 15:54:23 +0000

    ✨ Prevent playground page from indexing on SERP

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,5 +1,13 @@
 import { Scene } from '@/assets/shaders/scene';
 import { Container } from '@/components/container';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
 
 export default function Playground() {
   return (
