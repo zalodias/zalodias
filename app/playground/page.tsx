f4c814ec9d5a42c066ada0f566a650062966e217
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-03-23 23:09:41 +0000

    ✨ Improve playground card styling

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -17,11 +17,11 @@ export default function Playground() {
         A laboratory of digital experiments. Crafting and tinkering with code.
       </Intro>
       <div className="grid gap-10 md:grid-cols-[repeat(auto-fill,minmax(360px,1fr))]">
-        <div className='relative aspect-[4/3] bg-background-neutral-faded border border-border-neutral-faded rounded-lg'>
+        <div className='relative aspect-[4/3] bg-background-neutral-faded border border-border-neutral-faded rounded-lg overflow-hidden isolate after:absolute after:inset-x-0 after:bg-gradient-to-t after:from-background-neutral-default after:to-transparent after:h-1/4 after:bottom-0'>
           <Scene /> 
-          <div className='absolute flex justify-between inset-x-4 bottom-4'>
+          <div className='absolute flex z-20 justify-between inset-x-4 bottom-4'>
             <span className='text-body-medium-subtle'>Turbulence Shader</span>
-            <span className='text-body-medium-subtle text-foreground-neutral-faded'>March 2025</span>
+            <span className='text-body-medium-subtle text-foreground-neutral-subtle'>March 2025</span>
           </div>
         </div>
       </div>
