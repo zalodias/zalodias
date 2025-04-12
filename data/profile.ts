d248f1d660c7bce65f294adea9e5fa3c6be34f07
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-04-12 20:07:35 +0100

    📝 Update profile description

--- data/profile.ts
+++ data/profile.ts
@@ -13,7 +13,8 @@ export const profile: Profile = {
   name: 'Gonçalo Dias',
   handle: 'zalodias',
   title: 'Software Designer',
-  description: 'Software designer, crafting intuitive & polished digital experiences — from concept to code.',
+  description:
+    'Software designer crafting intuitive & polished digital experiences — from concept to code.',
   tagline: 'Making the world a better place, 1px at a time.',
   avatar: '/headshot@640.jpg',
   location: 'Coimbra',
