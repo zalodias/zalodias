c49b2a903dda8dbc967433e43836e24c0bb6a9b4
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-04-14 08:54:41 +0100

    📝 Update projects page intro

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,14 +13,14 @@ export default async function Projects() {
   return (
     <Container>
       <Intro title="Projects">
-        A showcase of my latest design work. Currently crafting thoughtful
-        digital experiences at{' '}
+        A showcase of my latest design work. Currently designing digital health
+        solutions at{' '}
         <a
-          href="https://www.concealed.com"
+          href="https://www.swordhealth.com"
           className="text-body-large-subtle text-foreground-brand-default hover:underline hover:underline-offset-2"
           target="_blank"
         >
-          Concealed
+          Sword Health
         </a>
         .
       </Intro>
