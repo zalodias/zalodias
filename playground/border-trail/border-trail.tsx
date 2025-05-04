dc0575da538d094099004e8ee40c077116b15c4b
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-05-04 11:19:24 +0100

    ✨ Improve border trail effect with themed styles

--- playground/border-trail/border-trail.tsx
+++ playground/border-trail/border-trail.tsx
@@ -3,12 +3,12 @@ import '@/playground/border-trail/border-trail.css';
 export function BorderTrail() {
   return (
     <div
-      className="relative overflow-clip rounded-[var(--radius)]"
+      className="relative overflow-clip rounded-[var(--radius)] shadow-sm"
       style={{ '--radius': '20px' } as React.CSSProperties}
     >
       <div className="absolute inset-0 -z-10 rounded-[var(--radius)]">
         <div
-          className="duration-4s absolute aspect-[2/1] w-[100px] rounded-[var(--radius)] bg-radial-[50%_50%_at_right] from-[hsl(160,96%,48%)] to-[transparent_50%]"
+          className="from-foreground-brand-default absolute aspect-[2/1] w-full rounded-[var(--radius)] bg-radial-[50%_50%_at_right] to-[transparent_50%]"
           style={{
             offsetPath: 'border-box',
             offsetAnchor: '100% 50%',
@@ -16,7 +16,7 @@ export function BorderTrail() {
           }}
         ></div>
       </div>
-      <div className="rounded-[20px] border-1 border-transparent bg-gradient-to-b from-[#222] to-[#000] bg-clip-padding px-4 py-1">
+      <div className="from-background-neutral-subtle to-background-neutral-default rounded-[var(--radius)] border-1 border-transparent bg-linear-to-b bg-clip-padding px-4 py-2">
         Get in touch
       </div>
     </div>
