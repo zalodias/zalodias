cd0e9865d3919530c8f93825493fd179e8fc6a0a
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-05-04 15:23:43 +0100

    ✨ Add OG image generation for main pages

---
+++ app/notes/opengraph-image.tsx
@@ -0,0 +1,26 @@
+import { Thumbnail } from '@/components/thumbnail';
+import { notes } from '@/data/metadata';
+import { ImageResponse } from 'next/og';
+import { readFile } from 'node:fs/promises';
+import { join } from 'node:path';
+
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = 'image/png';
+
+export default async function Image() {
+  const inter = await readFile(join(process.cwd(), 'assets/fonts/Inter.ttf'));
+
+  return new ImageResponse(<Thumbnail title={notes.title} />, {
+    ...size,
+    fonts: [
+      {
+        name: 'Inter',
+        data: inter,
+      },
+    ],
+  });
+}
