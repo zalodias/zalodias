9b51bc9d0d2814377dc2ae8f88fb1885fa4cafb9
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-06-15 18:03:32 +0100

    💄 Sort Tailwind classes

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ export default function Loading() {
     <div className="absolute inset-0 grid place-items-center lg:left-80">
       <div
         role="status"
-        className="size-8 animate-spin rounded-full border-2 border-solid border-e-transparent text-foreground-neutral-default"
+        className="text-foreground-neutral-default size-8 animate-spin rounded-full border-2 border-solid border-e-transparent"
       >
         <span className="sr-only">Loading</span>
       </div>
