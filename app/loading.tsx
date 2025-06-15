6f831bf2566cc0c00ce3ae805375e5e077d80248
Gonçalo Dias <goncalodias.studio@gmail.com>
2025-06-15 18:52:34 +0100

    🎨 Update loading indicator position based on sidebar width

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="absolute inset-0 grid place-items-center lg:left-80">
+    <div className="absolute inset-0 grid place-items-center lg:left-[var(--sidebar-width,20rem)]">
       <div
         role="status"
         className="text-foreground-neutral-default size-8 animate-spin rounded-full border-2 border-solid border-e-transparent"
