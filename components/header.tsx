e67f7dec77770260ac8ab0a714f705aa1f4c7cfb
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-08-03 09:18:39 +0100

    🎨 Remove profile active status indicator

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,21 @@
-import { Profile } from '@/components/profile';
+import { profile } from '@/data/profile';
 
 export function Header() {
   return (
-    <div className="sticky top-0 z-10 flex gap-5 border-b border-border-neutral-faded bg-linear-to-b from-background-neutral-default to-background-neutral-default/80 p-5 backdrop-blur-sm lg:hidden">
-      <Profile className="grow" />
+    <div className="border-border-neutral-faded from-background-neutral-default to-background-neutral-default/80 sticky top-0 z-10 flex gap-5 border-b bg-linear-to-b p-5 backdrop-blur-sm lg:hidden">
+      <div className="flex items-center gap-4">
+        <div className="relative shrink-0">
+          <img
+            src={profile.avatar}
+            alt={profile.name}
+            className="border-border-neutral-faded size-12 rounded-full border"
+          />
+        </div>
+        <div className="flex flex-col">
+          <p className="text-body-large-strong">{profile.name}</p>
+          <p className="text-foreground-neutral-faded">{profile.title}</p>
+        </div>
+      </div>
     </div>
   );
 }
