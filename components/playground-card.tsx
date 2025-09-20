f5fabbd4c7472ee6899ab16304e946bbd23593d6
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-09-20 15:50:36 +0100

    🎨 Improve  playground card layout

--- components/playground-card.tsx
+++ components/playground-card.tsx
@@ -24,25 +24,25 @@ export function PlaygroundCard({
 
   return (
     <div className="bg-background-neutral-faded border-border-neutral-faded after:from-background-neutral-default relative isolate grid aspect-[4/3] place-items-center overflow-hidden rounded-lg border after:absolute after:inset-x-0 after:bottom-0 after:h-1/4 after:bg-gradient-to-t after:to-transparent">
-      {restart && (
-        <button
-          onClick={handleRestart}
-          className="bg-background-neutral-default text-foreground-neutral-faded hover:text-foreground-neutral-default border-border-neutral-faded group absolute top-1 right-1 cursor-pointer rounded-lg border p-2 transition active:scale-95"
-        >
-          <RotateCw
-            className="size-4 transition-transform duration-400"
-            style={{ transform: `rotate(${rotation}deg)` }}
-          />
-        </button>
-      )}
       {Children.map(children, (child) => {
         if (isValidElement(child)) {
           return cloneElement(child, { key });
         }
         return child;
       })}
-      <div className="absolute inset-x-4 bottom-4 z-20 flex justify-between">
+      <div className="absolute inset-x-4 bottom-4 z-20 flex items-end justify-between">
         <span className="text-body-medium-subtle">{title}</span>
+        {restart && (
+          <button
+            onClick={handleRestart}
+            className="bg-background-neutral-default text-foreground-neutral-faded hover:text-foreground-neutral-default border-border-neutral-faded group absolute -right-2 -bottom-2 cursor-pointer rounded-lg border p-2 transition active:scale-95"
+          >
+            <RotateCw
+              className="size-4 transition-transform duration-400"
+              style={{ transform: `rotate(${rotation}deg)` }}
+            />
+          </button>
+        )}
       </div>
     </div>
   );
