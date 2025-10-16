c07e73c1a94968a72b2f1e2c7f62475c053c9a39
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-10-16 22:59:48 +0100

    💄 Sort Tailwind classes

--- components/project-metadata.tsx
+++ components/project-metadata.tsx
@@ -6,7 +6,7 @@ interface ProjectMetadataProps {
 export function ProjectMetadata({ title, description }: ProjectMetadataProps) {
   return (
     <div className="flex flex-col gap-2">
-      <p className="text-body-small-subtle uppercase text-foreground-neutral-faded">
+      <p className="text-body-small-subtle text-foreground-neutral-faded uppercase">
         {title}
       </p>
       <p className="text-body-medium-subtle">{description}</p>
