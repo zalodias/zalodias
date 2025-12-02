b4c6dadb3e32e7d7dc20b7aa31cec4cb3828b210
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2025-12-02 00:32:20 +0000

    💄 Improve spacing variable naming

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <Container className="min-h-[calc(100vh-var(--spacing-height-footer))] items-center justify-center lg:h-auto">
+    <Container className="min-h-[calc(100vh-var(--spacing-footer-height))] items-center justify-center lg:h-auto">
       <div className="flex flex-col items-center gap-5">
         <div className="grid gap-2 text-center">
           <h1 className="text-title-large-strong">404</h1>
