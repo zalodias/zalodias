af0ee28b51bf32cdbc260b6bfcf7638db6e6b0bf
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2026-02-04 09:19:24 +0000

    🎨 Adjust container bottom padding

--- components/container.tsx
+++ components/container.tsx
@@ -9,7 +9,7 @@ export function Container({ children, className }: ContainerProps) {
   return (
     <main
       className={cn(
-        'mx-auto flex w-full max-w-(--breakpoint-lg) grow flex-col gap-20 px-5 pt-10 pb-36 md:px-10 lg:w-[calc(100%-20rem)] lg:pb-10',
+        'mx-auto flex w-full max-w-(--breakpoint-lg) grow flex-col gap-20 px-5 pt-10 pb-36 md:px-10 lg:w-[calc(100%-20rem)] lg:pb-18',
         className,
       )}
     >
