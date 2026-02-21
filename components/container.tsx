1cbe6162caf07cbdd4adac9b2c9a663e91ea425c
Gonçalo Dias <63919176+zalodias@users.noreply.github.com>
2026-02-21 13:42:55 +0000

    🎨 Add responsive container gap spacing

--- components/container.tsx
+++ components/container.tsx
@@ -9,7 +9,7 @@ export function Container({ children, className }: ContainerProps) {
   return (
     <main
       className={cn(
-        'mx-auto flex w-full max-w-(--breakpoint-lg) grow flex-col gap-20 px-5 pt-10 pb-36 md:px-10 lg:w-[calc(100%-20rem)] lg:pb-18',
+        'mx-auto flex w-full max-w-(--breakpoint-lg) grow flex-col gap-15 px-5 pt-10 pb-36 md:gap-20 md:px-10 lg:w-[calc(100%-20rem)] lg:pb-18',
         className,
       )}
     >
