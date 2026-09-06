37aedb38ec4622a21cf1b7f6cf80766228c9402f
user.email <63919176+zalodias@users.noreply.github.com>
2026-09-06 17:56:27 +0100

    🎨 Remove redundant logo wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -74,7 +74,7 @@ export default async function Home() {
                 key={work.company}
                 className="flex flex-col items-start gap-4 py-2 md:flex-row md:items-center"
               >
-                <div className="overflow-hidden rounded-full">{work.image}</div>
+                {work.image}
                 <div className="flex grow flex-col">
                   <div className="group flex items-center gap-2">
                     <h4 className="text-body-large-strong">{work.company}</h4>
