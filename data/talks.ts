ac2133af455c95aaa018e2c43bca36beaabf1eb3
user.email <63919176+zalodias@users.noreply.github.com>
2026-09-13 11:59:57 +0100

    ✨ Expand talks catalog and allow entries without recordings

--- data/talks.ts
+++ data/talks.ts
@@ -2,14 +2,25 @@ export interface Talk {
   title: string;
   date: string;
   event: string;
-  link: string;
+  link?: string;
 }
 
 export const talks: Talk[] = [
   {
     title: 'Practical Color Systems in Figma',
     date: '2025-10-09',
-    event: 'Layers 2025',
+    event: 'Layers',
     link: 'https://youtu.be/b2Hzic3RxjA',
+  },
+  {
+    title: 'Bridging the Gap Between Design and Code',
+    date: '2025-07-09',
+    event: 'Friends of Figma World Tour',
+    link: 'https://youtu.be/2kW6-X6zmtc',
+  },
+  {
+    title: 'Using Notion as a Headless CMS',
+    date: '2025-06-12',
+    event: 'Coimbra.js',
   },
 ];
